5c6d73bfa599604ecec5db5e37bc8ee06336dc25
saniya-dogra <saniyadogra2@gmail.com>
2026-01-08 21:45:49 +0530

    Initial commit - Zoya Enterprises website

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Zoya Enterprises website loaded");
+
+// Example interaction
+document.querySelectorAll(".navbar a").forEach(link => {
+  link.addEventListener("click", () => {
+    alert("This section will be added soon!");
+  });
+});
