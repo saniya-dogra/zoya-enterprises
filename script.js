515ca0ee39b4b5861e5c299c003fb7acc1a6cc16
saniya-dogra <saniyadogra2@gmail.com>
2026-01-08 23:11:38 +0530

    Add files

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("Zoya Enterprises website loaded");
-
-// Example interaction
-document.querySelectorAll(".navbar a").forEach(link => {
-  link.addEventListener("click", () => {
-    alert("This section will be added soon!");
-  });
-});
