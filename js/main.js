fdcb9decc744479d5ab0486f809b038615748ccf
saniya-dogra <saniyadogra2@gmail.com>
2026-01-09 02:20:17 +0530

    Change UI

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Zoya Enterprises Website Loaded");
+let cartCount = 0;
+
+function addToCart() {
+  cartCount++;
+  document.getElementById("cart-count").innerText = cartCount;
+  alert("Item added to cart!");
+}
+
+function openCart() {
+  alert("Cart feature coming soon!");
+}
+
+function login() {
+  alert("Login / Signup coming soon!");
+}
+
+console.log("Zoya Enterprises Loaded");
